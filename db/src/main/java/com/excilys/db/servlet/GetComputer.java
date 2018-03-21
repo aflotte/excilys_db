98e6229933fa9097417f0aa54339710c5cc745b4
Flotte <excilys@localhost.localdomain>
2018-03-21 17:07:51 +0100

    PageDTO

--- db/src/main/java/com/excilys/db/servlet/GetComputer.java
+++ db/src/main/java/com/excilys/db/servlet/GetComputer.java
@@ -17,8 +17,8 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet("/GetComputer")
 public class GetComputer extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,31 +27,33 @@ public class GetComputer extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		ServletOutputStream out = response.getOutputStream();
-		
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        ServletOutputStream out = response.getOutputStream();
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-	
-	
-	public void service (ServletRequest req, ServletResponse res ) throws ServletException, IOException  {
-	res.setContentType( "text/html" );
-	PrintWriter out = res.getWriter();
-	
-	out.println( "<HTML>" );
-	
-	out.close();
-	}
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        response.setContentType( "text/html" );
+        PrintWriter out = response.getWriter();
+        doGet(request, response);
+    }
+
+
+    public void service (ServletRequest req, ServletResponse res ) throws ServletException, IOException  {
+        res.setContentType( "text/html" );
+        PrintWriter out = res.getWriter();
+
+        out.println( "<HTML>" );
+
+        out.close();
+    }
 
 }
