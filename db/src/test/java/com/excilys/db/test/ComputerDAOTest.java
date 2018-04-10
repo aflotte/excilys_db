6c44737e4457247bed6e9ab060358f5935eca09d
Flotte <excilys@localhost.localdomain>
2018-04-10 15:23:36 +0200

    [WIP] début test

--- db/src/test/java/com/excilys/db/test/ComputerDAOTest.java
+++ db/src/test/java/com/excilys/db/test/ComputerDAOTest.java
@@ -66,14 +66,14 @@ public class ComputerDAOTest extends TestCase {
                     "    PRIMARY KEY (id))");
             statement.executeUpdate("alter table computer add constraint fk_computer_company_1 foreign key (company_id) references company (id) on delete restrict on update restrict;");
             statement.executeUpdate("create index ix_computer_company_1 on computer (company_id);");
-            statement.executeUpdate("insert into company (id,name) values (  1,'Apple Inc.');");
-            statement.executeUpdate("insert into company (id,name) values (  2,'Thinking Machines');");
-            statement.executeUpdate("insert into company (id,name) values (  3,'RCA');");
-            statement.executeUpdate("insert into computer (id,name,introduced,discontinued,company_id) values (  1,'MacBook Pro 15.4 inch',null,null,1);");
-            statement.executeUpdate("insert into computer (id,name,introduced,discontinued,company_id) values (  2,'CM-2a',null,null,2);");
-            statement.executeUpdate("insert into computer (id,name,introduced,discontinued,company_id) values (  3,'CM-200',null,null,2);");
-            statement.executeUpdate("insert into computer (id,name,introduced,discontinued,company_id) values (  4,'CM-5e',null,null,2);");
-            statement.executeUpdate("insert into computer (id,name,introduced,discontinued,company_id) values (  5,'CM-5','1991-01-01',null,2);");
+            statement.executeUpdate("insert into company (id,name) values (  1,'Apple Inc.')");
+            statement.executeUpdate("insert into company (id,name) values (  2,'Thinking Machines')");
+            statement.executeUpdate("insert into company (id,name) values (  3,'RCA')");
+            statement.executeUpdate("insert into computer (id,name,introduced,discontinued,company_id) values (  1,'MacBook Pro 15.4 inch',null,null,1)");
+            statement.executeUpdate("insert into computer (id,name,introduced,discontinued,company_id) values (  2,'CM-2a',null,null,2)");
+            statement.executeUpdate("insert into computer (id,name,introduced,discontinued,company_id) values (  3,'CM-200',null,null,2)");
+            statement.executeUpdate("insert into computer (id,name,introduced,discontinued,company_id) values (  4,'CM-5e',null,null,2)");
+            statement.executeUpdate("insert into computer (id,name,introduced,discontinued,company_id) values (  5,'CM-5','1991-01-01',null,2)");
             logger.debug("executed_init");
         }catch (Exception e) {
             logger.warn(e.getMessage());
